8bc0a1c046d892fa890ab9f8fb05007799f60a11
Sara Oliviero <canadianheart23@gmail.com>
2020-10-21 11:43:06 +0200

    changed text

--- BellaFigura/BellaFiguraApp.swift
+++ BellaFigura/BellaFiguraApp.swift
@@ -15,3 +15,9 @@ struct BellaFiguraApp: App {
         }
     }
 }
+
+struct BellaFiguraApp_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
