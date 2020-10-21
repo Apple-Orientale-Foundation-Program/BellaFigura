ccb51a2d5962764c744e37d8eab4869865c3950b
StefaniaCaiazza <caiazza.stefania@gmail.com>
2020-10-21 11:36:40 +0200

    Initial Commit

---
+++ BellaFigura/BellaFiguraApp.swift
@@ -0,0 +1,17 @@
+//
+//  BellaFiguraApp.swift
+//  BellaFigura
+//
+//  Created by Stefania Caiazza on 21/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct BellaFiguraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BellaFigura/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BellaFigura
+//
+//  Created by Stefania Caiazza on 21/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
