620afa981652dfecddfa177de0615507872bc1eb
gabriborr <g.borriello2602@gmail.com>
2020-10-22 15:09:05 +0200

    Tab Bar

---
+++ BellaFigura/Views/CommunityView.swift
@@ -0,0 +1,20 @@
+//
+//  CommunityView.swift
+//  BellaFigura
+//
+//  Created by Gabriele Borriello on 22/10/2020.
+//
+
+import SwiftUI
+
+struct CommunityView: View {
+    var body: some View {
+        Text("Community")
+    }
+}
+
+struct CommunityView_Previews: PreviewProvider {
+    static var previews: some View {
+        CommunityView()
+    }
+}
