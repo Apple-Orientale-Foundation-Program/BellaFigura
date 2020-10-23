3f7dd42e0bfc9418f05bb9f4a0a0c7b60626bfec
Arianna Costagliola <arianna.costagliola@live.it>
2020-10-23 10:25:02 +0200

    progetto

--- BellaFigura/Views/QuizMenuView.swift
+++ BellaFigura/Views/QuizMenuView.swift
@@ -10,21 +10,16 @@ import SwiftUI
 struct QuizMenuView: View {
     var body: some View {
         NavigationView{
+            VStack {
+            Image("businessMan")
+                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
+                    .stroke()
+                    .fill(Color(#colorLiteral(red: 0.9294117647, green: 0.431372549, blue: 0.3411764706, alpha: 1)))
+                    .padding()
+                    .overlay(Text("vabbè comunque lascia stare")
+                                .foregroundColor(Color.black))
             
-            Form {
-                
             }
-            .navigationBarTitle("Quiz Menu", displayMode: .inline)
-            .navigationBarItems(trailing:
-                Button(
-                    action: {/*Inserire funzione Spiegazioni*/},
-                    label: {
-                        Image(
-                            systemName: "info.circle"
-                        ).accentColor(Color(#colorLiteral(red: 0.9215686275, green: 0.431372549, blue: 0.3411764706, alpha: 1)))
-                    }
-                )
-            )
         }
     }
 }
