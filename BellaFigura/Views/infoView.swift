5527f678dd5e16586feb0060ae4c541eb1c0ed24
Fabio Priore <fabio.fp@hotmail.it>
2020-10-23 14:53:34 +0200

    infoview

--- BellaFigura/Views/infoView.swift
+++ BellaFigura/Views/infoView.swift
@@ -9,16 +9,74 @@ import SwiftUI
 
 struct infoView: View {
     var body: some View {
-        VStack {
-        Image("QM")
-            .padding(.all, 30.0)
         
-        Text("Bella figura")
-            .padding()
+        NavigationView {
+        
+//inizio VStack
+        VStack {
+            
+        //IMAGE
+            Image("QM")
+            
+            
+            
+            
+            
+        //TEXT
+            Text("Making a \n\n 'Bella Figura'\n(good impression)\n\nis the key to creating social relationships and being able to interact as best as possible with the Italian community.\n\n Learn social norms with us\n so that you will not make\n a bad impression,\n “una Brutta Figura”!")
+                .multilineTextAlignment(.center)
+                .padding(.all, 30.0)
             .overlay(
             RoundedRectangle(cornerRadius: 16)
             .stroke(Color(#colorLiteral(red: 0.9215686275, green: 0.431372549, blue: 0.3411764706, alpha: 1)), lineWidth: 4))
-        }
+            
+            
+            
+            
+            
+            
+        //DONTSHOW
+            Button(action: {
+                    // do when checked
+                
+                
+                
+                
+                    
+            }) {
+                HStack(alignment: .top, spacing: 10) {
+
+                    Rectangle()
+                        .fill(Color.white)
+                        .frame(width:20, height:20, alignment: .center)
+                        .cornerRadius(5)
+                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                    Text("Don't show again")
+                        .accentColor(/*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/)
+                }
+            }
+            
+            
+        //button start
+            Button(action: {
+                NavigationLink(
+                    destination: ContentView(),
+                    label: {
+                        Text("Start")
+                    })
+                
+            
+            })
+            
+            {   Text("     Start     ")
+                .padding()
+                .accentColor(.white)
+                    .background(RoundedRectangle(cornerRadius: 13))
+                    .accentColor(Color(#colorLiteral(red: 0.9215686275, green: 0.431372549, blue: 0.3411764706, alpha: 1)))
+            }
+            
+        } .padding(.all) //fine Vstack
+        } //fine nav view
     }
 }
 
