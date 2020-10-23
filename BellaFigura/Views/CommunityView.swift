3166feacbc46057eff79d367396e093cb6c3b617
StefaniaCaiazza <caiazza.stefania@gmail.com>
2020-10-23 16:25:28 +0200

    CommunityView added

--- BellaFigura/Views/CommunityView.swift
+++ BellaFigura/Views/CommunityView.swift
@@ -9,7 +9,42 @@ import SwiftUI
 
 struct CommunityView: View {
     var body: some View {
-        Text("Community")
+        NavigationView{
+            ZStack{
+                Image("communityBackground")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                //                        .scaledToFill()
+                //                        .edgesIgnoringSafeArea(.all)
+                VStack{
+                    Text("""
+Have you ever found yourself in an awkward situation and you didn’t know what to do or how to behave? Write us an e-mail and let us know!
+We will add your experience to our collection!
+""")
+                        .fixedSize(horizontal: false, vertical: true)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                        .frame(width: 300, height: 200, alignment: .center)
+                        .background(RoundedRectangle(cornerRadius: 13)
+                                        .strokeBorder(Color(#colorLiteral(red: 0.9294117647, green: 0.431372549, blue: 0.3411764706, alpha: 1)), lineWidth: 4)
+                                        .background(Color(.white))
+                        )
+                    Button(action: {}, label: {
+                        Text("E-mail")
+                            .font(.title3)
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(RoundedRectangle(cornerRadius: 13)
+                                            .fill(Color(#colorLiteral(red: 0.9294117647, green: 0.431372549, blue: 0.3411764706, alpha: 1)))
+                                            .frame(width: 165, height: 50)
+                            )
+                        
+                    }
+                    )
+                }
+            }
+            .navigationBarTitle(("Community"), displayMode: .inline)
+        }
     }
 }
 
