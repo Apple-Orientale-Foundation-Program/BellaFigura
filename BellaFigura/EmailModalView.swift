0f3a9db0242561b9ecb4c8b859acb2539572d766
Arianna Costagliola <arianna.costagliola@live.it>
2020-10-23 16:55:39 +0200

    email modal 2

--- BellaFigura/EmailModalView.swift
+++ BellaFigura/EmailModalView.swift
@@ -8,34 +8,35 @@
 import SwiftUI
 
 struct EmailView: View {
+    @Binding var showModal: Bool
     var body: some View {
         NavigationView {
             List {
                 HStack{
-                Text("To:")
+                    Text("To:")
                     Text("teambellafigura@gmail.com")
-                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.blue)
                 }
                 Text("Cc/Bcc:")
                 Text("Subject:")
             }.foregroundColor(.gray)
             
-         
+            
             .navigationBarTitle("Tell us your story!", displayMode: .inline)
             .navigationBarItems(trailing:
-            Button(
-                action: {},
-                label: {
-                    Text("Send")
-                        .accentColor(Color(#colorLiteral(red: 0.9294117647, green: 0.431372549, blue: 0.3411764706, alpha: 1)))
-                }
-            )
+                                    Button(
+                                        action: {},
+                                        label: {
+                                            Text("Send")
+                                                .accentColor(Color(#colorLiteral(red: 0.9294117647, green: 0.431372549, blue: 0.3411764706, alpha: 1)))
+                                        }
+                                    )
             )
         }
     }
 }
 struct EmailView_Previews: PreviewProvider {
     static var previews: some View {
-        EmailView()
+        EmailView(showModal: .constant(true))
     }
 }
