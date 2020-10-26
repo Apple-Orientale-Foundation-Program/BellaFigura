e6f3b7b010aa149544be71597ef5341dcd0628d1
Arianna Costagliola <arianna.costagliola@live.it>
2020-10-26 10:51:47 +0100

    Modifica

---
+++ BellaFigura/UniversityQuizIntroduction.V.swift
@@ -0,0 +1,20 @@
+//
+//  UniversityQuizIntroduction.V.swift
+//  BellaFigura
+//
+//  Created by Arianna Costagliola on 26/10/2020.
+//
+
+import SwiftUI
+
+struct UniversityQuizIntroduction_V: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UniversityQuizIntroduction_V_Previews: PreviewProvider {
+    static var previews: some View {
+        UniversityQuizIntroduction_V()
+    }
+}
