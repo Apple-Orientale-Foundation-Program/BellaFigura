f62e2f71eec3215dae368325b6c246f671d0f45c
Fabio Priore <fabio.fp@hotmail.it>
2020-10-26 11:57:31 +0100

    splash view

--- BellaFigura/SplashView.swift
+++ BellaFigura/SplashView.swift
@@ -7,12 +7,35 @@
 
 import SwiftUI
 
-struct SplashView: View {
-    var body: some View {
-        Image("logo")
+    struct SplashView: View {
+        
+        // 1.
+        @State var isActive:Bool = false
+        
+        var body: some View {
+            VStack {
+                // 2.
+                if self.isActive {
+                    // 3.
+                    infoView()
+                } else {
+                    // 4.
+                    Image("logo")
+                }
+            }
+            // 5.
+            .onAppear {
+                // 6.
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                    // 7.
+                    withAnimation {
+                        self.isActive = true
+                    }
+                }
+            }
+        }
+        
     }
-}
-
 struct SplashView_Previews: PreviewProvider {
     static var previews: some View {
         SplashView()
